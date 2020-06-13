b3dcfb0f99d622b4e7f6c6954e7239150ab43d9a
ForeverOpp <life.app.hanfeng@hotmail.com>
2020-06-14 00:49:11 +0800

    首次测试，大部分能用

---
+++ getter.py
@@ -0,0 +1,36 @@
+import requests
+from urllib import parse
+import json
+
+class getter:
+    __list = ["API4","API1"]
+    def get(self,arg):
+        result = []
+        for i in self.__list:
+            result.append(eval("self."+i)(arg))
+        return result
+
+
+    def API1(self,a):
+        url = "http://qs.nnarea.cn/chaoxing_war/topicServlet?action=query&q="
+        r = ""
+        for i in range(0,3):
+            r += json.loads(requests.post(url + parse.quote(a['q']), 'course=' + parse.quote(a['curs']) + '&type=' + str(i)).text)['data']
+        return r
+
+    def API2(self,a):
+        url = "http://cx.icodef.com/wyn-nb"
+        r = ""
+        for i in range(0, 4):
+            r += requests.post(url, "question=" + parse.quote(a['q']) + '&type=' + str(i)).text
+        return r
+
+    def API3(self,a):
+        url = "http://cx.beaa.cn/cx.php"
+        r = requests.post(url, "content=" + parse.quote(a['q'])).text
+        return r
+
+    def API4(self,a):
+        url = "https://api3.4n0a.cn/jsapi.php?"
+        r = json.loads(requests.get(url+"q="+a['q']+"&token="+a['token']).text)
+        return r['da']
--- main.py
+++ main.py
@@ -1,2 +1,52 @@
 import pyperclip
-text=pyperclip.paste()
+import requests
+import json
+import execjs
+import js2py
+from urllib import parse
+import numpy
+import getter
+from win32api import MessageBox
+from win32con import MB_OK
+import os
+import time
+
+__header = {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'Authorization': ""
+}
+
+__token = ""
+
+
+def Output(message):
+    # MessageBox(0, message, "答案", MB_OK)
+    print(message)
+
+
+# clip = pyperclip.paste()
+
+
+def findAnswer(a):
+    courseid = "206267220"
+    g = getter.getter()
+    str = ""
+    j = ['I', 'II', 'III', 'IV']
+    h = 0
+    for i in g.get({'q': a, 'curs': courseid, 'type': "1", 'token': __token}):
+        str += j[h] + ". : " + i + "\n"
+        str += '-' * 20
+        str += "\n"
+        h += 1
+    Output(str)
+    str = ""
+    time.sleep(5)
+
+
+last_string = pyperclip.paste()
+while True:
+    time.sleep(0.1)
+    string = pyperclip.paste()
+    if string != last_string and string != '':
+        last_string = string
+        findAnswer(last_string)
